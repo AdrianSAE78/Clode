c4bfdbbff771ccb5dc372e050bb2040e71892839
Edwin Cacuango <edwin-fernando@hotmail.es>
2024-12-01 18:16:05 -0500

    cambios para uso de db en local

--- django_clode_api/settings.py
+++ django_clode_api/settings.py
@@ -102,8 +102,8 @@ DATABASES = {
         'NAME': 'clodeDB',
         'HOST': 'localhost',
         'PORT': '5432',
-        'USER': 'postgres',
-        'PASSWORD': 'admin'
+        'USER': 'ewin',
+        'PASSWORD': ''
     }
 }
 
