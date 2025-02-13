ea602ff27ddcd6fff08528b4de59cc24f4f5ff21
Edwin Cacuango <edwin-fernando@hotmail.es>
2025-02-13 10:16:30 -0500

    Estilos finalizados pagina login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,26 @@
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+import "../styles/login.css";
+import GoogleIcon from "../assets/google-icon.png";
 
-export default Login
+const Login = () => {
+  const loginHandler = () => {
+    return "hola";
+  };
+  return (
+    <div className="body-custom">
+      <div>
+        <h1 className="display-large title">InterMod</h1>
+        <h2 className="headline-large">Bienvenido de nuevo</h2>
+        <p className="body-large description">Ingresa mediante tu cuenta de Google</p>
+
+        <button className="btn-auth headline-small" onClick={loginHandler}>
+          <div className="icon">
+            <img src={GoogleIcon} />
+          </div>
+          <p>Ingresa con Google</p>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
